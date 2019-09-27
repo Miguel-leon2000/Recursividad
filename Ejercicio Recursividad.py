6c49823d346a94b1cd16e644d1534ac53a570ea7
Miguel Angel <leonaguirrem3@gmail.com>
2019-09-26 20:26:48 -0500

    Se definen la secuencia de numeros enteros en una lista (a) y las sumas de cada digito se colocan en una nueva lista (c), utilizando el metodo recursivo.

--- Ejercicio Recursividad.py
+++ Ejercicio Recursividad.py
@@ -5,3 +5,12 @@ class Recursividad:
             return num
         else:
             return (int((num / 10) / 10)) + (int((num / 10) % 10)) + num % 10
+
+Rec = Recursividad()
+a = [687, 135, 439, 852, 363]
+c = []
+
+for b in range(0, len(a)):
+    g = Rec.SepararNumeros(a[b])
+    c.append(g)
+print(c)
